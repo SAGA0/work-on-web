05fadb0de693b7e6e5fea732f8346a8fd5197c41
Ramzan <murdagaliev95@gmail.com>
2021-09-14 18:27:08 +0300

    improved code

--- js/script.js
+++ js/script.js
@@ -10,12 +10,25 @@ const personalMovieDB = {
 	privat: false,
 }
 
-const a = prompt('Один из просмотренных фильмов?', ''),
-	b = +prompt('На сколько оцените?', ''),
-	c = prompt('Один из просмотренных фильмов?', ''),
-	d = +prompt('На сколько оцените?', '')
+if (personalMovieDB.count <= 10) {
+	alert('Мало просмотренных фильмов')
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+	alert('Вы классический зритель')
+} else {
+	alert('Вы киноман')
+}
 
-personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из просмотренных фильмов?', ''),
+		b = +prompt('На сколько оцените?', '')
+
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMovieDB.movies[a] = b
+		console.log('done')
+	} else {
+		console.log('error')
+		i--
+	}
+}
 
 console.log(personalMovieDB)
