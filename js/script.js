1824345779496267dd36de46ea40e77c64f7811a
Ramzan <murdagaliev95@gmail.com>
2021-09-16 14:40:24 +0300

    added main functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 'use strict'
 
-const numberOfFilms = +prompt('Сколько фильмов вы смотрели', '')
+let numberOfFilms
+
+function start() {
+	numberOfFilms = +prompt('Сколько фильмов вы смотрели', '')
+
+	while (
+		numberOfFilms == '' ||
+		numberOfFilms == null ||
+		isNaN(numberOfFilms)
+	) {
+		numberOfFilms = +prompt('Сколько фильмов вы смотрели', '')
+	}
+}
+
+start()
 
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -10,25 +24,33 @@ const personalMovieDB = {
 	privat: false,
 }
 
-if (personalMovieDB.count <= 10) {
-	alert('Мало просмотренных фильмов')
-} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
-	alert('Вы классический зритель')
-} else {
-	alert('Вы киноман')
-}
-
-for (let i = 0; i < 2; i++) {
-	const a = prompt('Один из просмотренных фильмов?', ''),
-		b = +prompt('На сколько оцените?', '')
-
-	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-		personalMovieDB.movies[a] = b
-		console.log('done')
+function detectPersonalLevel() {
+	if (personalMovieDB.count <= 10) {
+		alert('Мало просмотренных фильмов')
+	} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+		alert('Вы классический зритель')
 	} else {
-		console.log('error')
-		i--
+		alert('Вы киноман')
 	}
 }
+
+detectPersonalLevel()
+
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++) {
+		const a = prompt('Один из просмотренных фильмов?', ''),
+			b = +prompt('На сколько оцените?', '')
+
+		if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+			personalMovieDB.movies[a] = b
+			console.log('done')
+		} else {
+			console.log('error')
+			i--
+		}
+	}
+}
+
+rememberMyFilms()
 
 console.log(personalMovieDB)
